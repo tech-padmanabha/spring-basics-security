e601bba173cc8b4911054b27793b010b4a622e01
padmanabha <spadm22@gmail.com>
2024-09-18 18:17:46 +0530

    remove the warnings

--- src/main/java/io/pn/controller/NormalController.java
+++ src/main/java/io/pn/controller/NormalController.java
@@ -9,13 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.pn.dto.RegisterRequest;
 import io.pn.dto.RegisterResponse;
-import io.pn.entity.AppUserDetails;
-import io.pn.repository.AppUserDetailsRepository;
 import io.pn.service.AppUserService;
 
 @RestController
